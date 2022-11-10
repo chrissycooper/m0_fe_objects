bf240a9adeee9dd347b11fa18fccda08927df540
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-11-10 15:38:18 -0600

    Clean up syntax

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,33 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
-
-var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+  eggs: 48
 }
+//I added some spacing to make it more readable by bumping things down a line. This way all the key/value pairs are in the same column together, and its clear to see where the object ends by bumping the curly bracket down to match with the `var` indent.
+
+var person = { 
+  height: "6 ft",
+  weight: "160 lbs" 
+}
+
+//similar here, bumped them down do you can quickly see how many key/value pairs there are
+
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+}
+
+//So i originally thought this was two different objects since the information was so far apart and oddly clustered! There was a lot of wrangling to get the indents to match.
+
+
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+}
+//bumped up the opening bracket, and bumped over the keys since they were weirdly far away.
